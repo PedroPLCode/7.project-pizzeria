d1b4162808544eeb07bda4c99a3bd4d242cbedda
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2023-07-21 11:56:46 +0200

    Update package.json, Add app.fetch() and json-server features

--- dist/js/script.js
+++ dist/js/script.js
@@ -71,6 +71,11 @@
     cart: {
       defaultDeliveryFee: 20,
     },
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
   
   const templates = {
@@ -532,13 +537,29 @@
       const thisApp = this;
 
       for (let productData in thisApp.data.products) {
-        new Product (productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function() {
       const thisApp = this;
-      thisApp.data = dataSource;
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.products;
+
+      fetch(url) 
+      .then(function(rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse) {
+        console.log('parsed response', parsedResponse);
+
+        thisApp.data.products = parsedResponse;
+
+        thisApp.initMenu();
+      });
+
+      console.log('json data', JSON.stringify(thisApp.data));
     },
 
     initCart: function() {
@@ -552,7 +573,7 @@
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
+      //thisApp.initMenu();
       thisApp.initCart();
     },
   };
